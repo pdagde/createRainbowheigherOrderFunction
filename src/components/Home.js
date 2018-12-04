644eafd5f3f384ddb1be4c9d20b3c7e76458141b
pdagde <pravin.dagde@knorex.com>
2018-12-04 15:50:58 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>In computing, React is a JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications. Wikipedia</p>
+        </div>
+    )
+}
+export default Home
